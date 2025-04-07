08187744f48d9d4b7ac7589a8cec525315486aef
Rewinell Guardiano <113446765+RewGuardiano@users.noreply.github.com>
2025-04-07 18:48:52 +0100

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,8 +1,4 @@
 # Output the public IP of the EC2 instance
-output "instance_public_ip" {
-  description = "The public IP address of the EC2 instance"
-  value       = aws_instance.app_server.public_ip
-}
 
 # Output the instance ID
 output "instance_id" {
